6e87bc9f5e742526ef8a3ca0919235daeade5a15
Alex Zhou <alexander.haoyang.zhou@gmail.com>
2022-11-26 18:03:17 -0500

    Initial Commit

---
+++ Shared/TesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesterApp.swift
+//  Shared
+//
+//  Created by Alex Zhou on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
