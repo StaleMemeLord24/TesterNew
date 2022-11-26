c861c81ecde10150ff6ed0e62b77ee53c86949b8
Alex Zhou <alexander.haoyang.zhou@gmail.com>
2022-11-26 18:18:45 -0500

    Speech recognition undedited file

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello!")
-            .padding()
+        NavigationView{
+            NavigationLink(destination: ActualInterface()){
+                Text("Start a new session")
+            }
+        }
     }
 }
 
